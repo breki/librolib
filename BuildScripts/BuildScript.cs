a554055926f674316ce07e92f3ccdea244f71d75
igor <226863+breki@users.noreply.github.com>
2015-11-30 11:12:15 +0100

    Added the build script project.

---
+++ BuildScripts/BuildScript.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Flubu;
+using Flubu.Builds;
+using Flubu.Builds.Tasks.AnalysisTasks;
+using Flubu.Builds.Tasks.TestingTasks;
+using Flubu.Targeting;
+
+//css_ref Flubu.dll;
+//css_ref Flubu.Contrib.dll;
+
+namespace BuildScripts
+{
+    public class BuildScript
+    {
+        public static int Main(string[] args)
+        {
+            DefaultBuildScriptRunner runner = new DefaultBuildScriptRunner(ConstructTargets, ConfigureBuildProperties);
+            return runner.Run(args);
+        }
+
+        private static void ConstructTargets (TargetTree targetTree)
+        {
+            targetTree.AddTarget("clean.output.debug")
+                .DependsOn("load.solution")
+                .Do(c =>
+                        {
+                            c.Properties.Set(BuildProps.BuildConfiguration, "Debug");
+                            targetTree.GetTarget("clean.output").Execute(c);
+                        });
+
+            targetTree.AddTarget ("rebuild")
+                .SetAsDefault ()
+                .SetDescription ("Builds the library and runs tests on it")
+                .DependsOn ("compile", "dupfinder", "tests");
+
+            targetTree.AddTarget("release")
+                .SetDescription ("Builds the library, runs tests on it and publishes it on the NuGet server")
+                .DependsOn ("rebuild");
+
+            targetTree.GetTarget ("fetch.build.version")
+                .Do (TargetFetchBuildVersion);
+
+            targetTree.AddTarget ("dupfinder")
+                .SetDescription ("Runs R# dupfinder to find code duplicates")
+                .Do (TargetDupFinder);
+
+            targetTree.AddTarget("tests")
+                .SetDescription("Runs tests on the project")
+                .Do (r =>
+                    {
+                        TargetRunTestsWithCoverage(r, "LibroLib.Tests");
+                    }).DependsOn ("load.solution");
+        }
+
+        private static void ConfigureBuildProperties (TaskSession session)
+        {
+            session.Properties.Set (BuildProps.CompanyName, "Igor Brejc");
+            session.Properties.Set (BuildProps.CompanyCopyright, "Copyright (C) 2010-2015 Igor Brejc");
+            session.Properties.Set (BuildProps.ProductId, "LibroLib");
+            session.Properties.Set (BuildProps.ProductName, "LibroLib");
+            session.Properties.Set (BuildProps.SolutionFileName, "LibroLib.sln");
+            session.Properties.Set (BuildProps.TargetDotNetVersion, FlubuEnvironment.Net40VersionNumber);
+            session.Properties.Set (BuildProps.VersionControlSystem, VersionControlSystem.Mercurial);
+        }
+
+        private static void TargetFetchBuildVersion (ITaskContext context)
+        {
+            Version version = BuildTargets.FetchBuildVersionFromFile (context);
+            version = new Version (version.Major, version.Minor, BuildTargets.FetchBuildNumberFromFile (context));
+            context.Properties.Set (BuildProps.BuildVersion, version);
+            context.WriteInfo ("The build version will be {0}", version);
+        }
+
+        private static void TargetDupFinder (ITaskContext context)
+        {
+            RunDupFinderAnalysisTask task = new RunDupFinderAnalysisTask ();
+            task.Execute (context);
+        }
+
+        private static void TargetRunTestsWithCoverage (ITaskContext context, string projectName)
+        {
+            NUnitWithDotCoverTask task = NUnitWithDotCoverTask.ForProject (
+                projectName,
+                @"packages\NUnit.Runners.2.6.4\tools\nunit-console.exe");
+            task.DotCoverFilters = "-:module=*.Tests;-:class=*Contract;-:class=*Contract`*";
+            task.FailBuildOnViolations = false;
+            task.Execute (context);
+        }
+   }
+}
---
+++ LibroLib.Tests/CommonTests/StringExTests.cs
@@ -0,0 +1,38 @@
+using System.Globalization;
+using NUnit.Framework;
+
+namespace LibroLib.Tests.CommonTests
+{
+    public class StringExTests
+    {
+        [Test]
+        [TestCase("   test ", "test", 3, 1)]
+        [TestCase("   test", "test", 3, 0)]
+        [TestCase("   test\r", "test", 3, 1)]
+        public void TrimExTests(string text, string trimmed, int start, int end)
+        {
+            int s, e;
+            string actualTrimmed = text.TrimEx(out s, out e);
+            Assert.AreEqual(trimmed, actualTrimmed);
+            Assert.AreEqual(start, s);
+            Assert.AreEqual(end, e);
+        }
+
+        [Test]
+        public void ConcatTest()
+        {
+            int[] values = new[] { 10, 20, 30 };
+            Assert.AreEqual("10, 20, 30", values.Concat(x => x.ToString(), ", "));
+        }
+
+        [Test]
+        [TestCase("pascal_case", "pascalCase")]
+        [TestCase("camel_case", "CamelCase")]
+        [TestCase("dbname", "DBName")]
+        [TestCase("new_dbname", "newDBName")]
+        public void UnderscoreNames(string expected, string original)
+        {
+            Assert.AreEqual(expected, original.ToUnderscoreName(CultureInfo.InvariantCulture));
+        }
+    }
+}
