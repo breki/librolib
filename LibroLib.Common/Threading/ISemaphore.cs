b2226790237d80a507cf61fe447da011d9d12878
igor <igor@localhost>
2015-11-30 22:21:44 +0100

    Imported threading code.

---
+++ LibroLib.Common/Threading/ISemaphore.cs
@@ -0,0 +1,8 @@
+﻿namespace LibroLib.Threading
+{
+    public interface ISemaphore : IWaitHandle
+    {
+        int Release();
+        int Release(int releaseCount);
+    }
+}
---
+++ LibroLib.Common/Threading/ISynchronizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LibroLib.Threading
+{
+    public interface ISynchronizer
+    {
+        void AssignWaitHandles(params IWaitHandle[] handles);
+        int WaitAny();
+        int WaitAny(TimeSpan timeout);
+        bool WaitAll();
+        bool WaitAll(TimeSpan timeout);
+    }
+}
