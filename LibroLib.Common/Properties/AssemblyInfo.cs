c1d6c78c45ee23926304bba88f8eccce2d243130
igor <igor@localhost>
2015-12-26 12:59:17 +0100

    1.0.2.2 - marked assemblies as CLS-compliant

--- LibroLib.Common/Properties/AssemblyInfo.cs
+++ LibroLib.Common/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -10,3 +11,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("22b9ab4e-7ad5-4c23-aa51-5bdb0fe74038")]
+
+[assembly: CLSCompliant(true)]
