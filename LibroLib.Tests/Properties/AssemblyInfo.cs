97e06ae63cd23c4422e0b907ffc5790220af0480
igor <226863+breki@users.noreply.github.com>
2018-02-23 13:39:59 +0100

    Converted LibroLib.Tests to .NET Standard

--- LibroLib.Tests/Properties/AssemblyInfo.cs
+++ LibroLib.Tests/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LibroLib.Tests")]
+//[assembly: AssemblyTitle("LibroLib.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCulture("")]
 
